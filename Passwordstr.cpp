0ae3f10d7d6691c9c7d20ab9f071f0f0893a3eca
Jagadheesh@spoidy <92840518+jagadheesh-as-berlin10@users.noreply.github.com>
2022-03-30 22:25:12 +0530

    Update Passwordstr.cpp

--- Passwordstr.cpp
+++ Passwordstr.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<conio.h>
-#include<dos.h> 
+#include<dos.h>
 #include <windows.h>
 #include <time.h>
 #include <string.h>
@@ -23,9 +23,9 @@ void drawBorder(int x=0, int y=0){
     char br = 188; // bottom right border
     char tl = 201; // top left border
     char bl = 200; // bottom left border
-    int width = 30; 
+    int width = 30;
     int height = 3;
-    
+
     for( int i=1; i<=height; i++ ){
 		for( int j=1; j<=width; j++ ){
 	    	gotoxy(j+x,i+y);
@@ -41,48 +41,48 @@ void drawBorder(int x=0, int y=0){
 
 int main() {
     srand((unsigned) time(NULL));
-	
+
 	system("cls");
- 	  
+
  	char password[21];
  	int length;
- 	
+
 	char op;
 	do{
 		system("cls");
-		
-		cout<<"�������������������������������������������"<<endl;
+
+		cout<<"±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±"<<endl;
 		cout<<endl;
-		cout<<"\t\t  ����� "<<endl;
-		cout<<"\t\t  �   � "<<endl;
-		cout<<"\t\t���������"<<endl;
-		cout<<"\t\t���� ����"<<endl;
-		cout<<"\t\t���������"<<endl; 
-		
+		cout<<"\t\t  ±±±±± "<<endl;
+		cout<<"\t\t  ±   ± "<<endl;
+		cout<<"\t\t±±±±±±±±±"<<endl;
+		cout<<"\t\t±±±± ±±±±"<<endl;
+		cout<<"\t\t±±±±±±±±±"<<endl;
+
 		cout<<endl;
 		cout<<"        PASSWORD  STRENGTH  CHECKER      "<<endl;
-		cout<<"�������������������������������������������"<<endl<<endl; 
-		  
+		cout<<"±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±"<<endl<<endl;
+
 		cout<<endl<<"\tEnter Password: ";
 		cin>> password;
-			   
+
 		char pass[21] = "";
 		int excess = strlen(password) - 6;
 		int baseScore = 50;
-		
+
 		int numUpper = 0;
 		int numSymbol = 0;
 		int numLower = 0;
 		int numDigit = 0;
-		
+
 		int bonusExcess = 3;
 		int bonusUpper = 4;
 		int bonusNumbers = 5;
 		int bonusSymbols = 5;
-		int bonusCombo = 0; 
+		int bonusCombo = 0;
 		int bonusFlatLower = 0;
 		int bonusFlatNumber = 0;
-		
+
 		int i;
 		for( i=0; i<strlen(password); i++ ){
 			if( password[i] >= 48 && password[i] <= 57 ){  // 0-9
@@ -98,52 +98,52 @@ int main() {
 				numSymbol++;
 			}
 		}
-		
+
 		if (numUpper>0 && numDigit>0 && numSymbol>0)
 		{
-			bonusCombo = 25; 
+			bonusCombo = 25;
 		}
-	
+
 		else if ((numUpper>0 && numDigit>0) || (numUpper>0 && numSymbol>0) || (numDigit>0 && numSymbol>0))
 		{
-			bonusCombo = 15; 
+			bonusCombo = 15;
 		}
-		
-		int score = baseScore + (excess*bonusExcess) + (numUpper*bonusUpper) + 
-				(numDigit*bonusNumbers) + (numSymbol*bonusSymbols) + 
-				bonusCombo + bonusFlatLower + bonusFlatNumber;	
-		 
-		gotoxy(8,14); 
+
+		int score = baseScore + (excess*bonusExcess) + (numUpper*bonusUpper) +
+				(numDigit*bonusNumbers) + (numSymbol*bonusSymbols) +
+				bonusCombo + bonusFlatLower + bonusFlatNumber;
+
+		gotoxy(8,14);
 		cout<<"Checking Password Strength...";
-		gotoxy(8,15); 
+		gotoxy(8,15);
 		for(int i=0; i<27; i++){
 			cout<<(char)176;
 			Sleep(100);
 		}
-		  
+
 		drawBorder(7,17);
-		
-		gotoxy(10,19); 
+
+		gotoxy(10,19);
 		if (score<50)
 		{
-			cout<<"Password Status: Weak"; 
+			cout<<"Password Status: Weak";
 		}
 		else if (score>=50 && score<75)
 		{
-			cout<<"Password Status: Average"; 
+			cout<<"Password Status: Average";
 		}
 		else if (score>=75 && score<100)
 		{
-			cout<<"Password Status: Strong"; 
+			cout<<"Password Status: Strong";
 		}
 		else if (score>=100)
 		{
-			cout<<"Password Status: Secure"; 
-		} 
-		
+			cout<<"Password Status: Secure";
+		}
+
 		gotoxy(9,23); cout<<"Do you want to generate password again (y/n): ";
 		op = getch();
 	}while(op=='y' || op=='Y');
-	
+
     return 0;
 }
